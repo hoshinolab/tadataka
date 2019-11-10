b0b3e49847dc21d047905ae05a1d52139af6db13
ryo-a <ryoa.dev@gmail.com>
2019-11-11 02:43:34 +0900

    add stdby subcommand

---
+++ cmd/stdby.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(stdbyCmd)
+}
+
+var stdbyCmd = &cobra.Command{
+	Use:   "stdby",
+	Short: "Load data to Redis",
+	Run: func(cmd *cobra.Command, args []string) {
+		//TODO get version data from another file
+		fmt.Println("LOAD DATA TO REDIS")
+	},
+}
