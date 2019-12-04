9bbf4fd8d047c3c8c9525f5d4d0e65dc18994793
ryo-a <ryoa.dev@gmail.com>
2019-12-04 22:21:08 +0900

    WIP grid handler

--- util/grid_handler.go
+++ util/grid_handler.go
@@ -8,9 +8,13 @@ import (
 
 type Grids struct {
 	East       string
-	West       string
-	South      string
+	NorthEast  string
 	North      string
+	NorthWest  string
+	West       string
+	SouthWest  string
+	South      string
+	SouthEast  string
 	UpperLevel string
 }
 
@@ -60,6 +64,7 @@ var decrementNeighborCode map[string]string = map[string]string{
 	"X": "W",
 }
 
+// only support "8Q7XPX2P+" level
 func GetNeighborGrid(grid string) (g Grids) {
 	///2,3,4,5,6,7,8,9,C,F,G,H,J,M,P,Q,R,V,W,X
 	//Validation
@@ -67,12 +72,58 @@ func GetNeighborGrid(grid string) (g Grids) {
 		fmt.Println(err)
 		return
 	}
+	//8Q7XPX2P+J4 -> 8Q 7X PX 2P
+	/*containsPlus := false
+	if strings.Contains(grid, "+") {
+		containsPlus = true
+		grid = grid[:len(grid)-1] //remove "+"
+	}*/
 
-	//x axis and y axis
-	//x := "2"
-	//y := "2"
+	// 1段階 TODO 書き直す
+	// 8Q7XPX2P+J4 -> 8Q 7X PX 2Pの分割やって 4th 3rd 2nd 1st で下から判定してそれぞれ隣接取る
+	// X, Y 別々に取るほうが良さそう
 
-	//TODO Detection Level
+	firstLvY := grid[:1]
+	firstLvX := grid[1:2]
+	secondLvY := grid[2:3]
+	secondLvX := grid[3:4]
+	thirdLvY := grid[4:5]
+	thirdLvX := grid[5:6]
+	fourthLvY := grid[6:7]
+	fourthLvX := grid[7:8]
+
+	//4th
+	//fourthNorthY := incrementNeighborCode[fourthLvY]
+	//fourthSouthY := decrementNeighborCode[fourthLvY]
+	if fourthLvY == "X" {
+		// TODO check 3rd
+		if thirdLvY == "X" {
+			// TODO check 2nd
+			if secondLvY == "X" {
+				// TODO check 1st
+				if firstLvY == "X" {
+					// TODO check
+				}
+			}
+		}
+	}
+	//TODO also chrck 2
+
+	if fourthLvX == "X" || fourthLvX == "2" {
+		// TODO check 3rd
+		if thirdLvX == "X" || thirdLvX == "2" {
+			// TODO check 2nd
+			if secondLvX == "X" || secondLvX == "2" {
+				// TODO check 1st
+				if firstLvX == "X" || firstLvX == "2" {
+					// TODO check
+				}
+			}
+		}
+	}
+
+	//test[len(test)-2:len(test)]
+
 	//TODO 端部だったら上位グリッドも取得
 	return
 }
