8b58b3f4266154de1618553e65cc7e76c176f277
ryo-a <ryoa.dev@gmail.com>
2019-12-09 21:18:34 +0900

    modify for alpha release

--- status/status_checker.go
+++ status/status_checker.go
@@ -1,14 +1,5 @@
 package status
 
-import (
-	"fmt"
-	"tadataka/db"
-)
-
 func RedisCheck() {
-	s1 := db.GetMembersFromList("8Q6MJWGC+", "ISJ")
-	s2 := db.GetMembersFromList("8Q6MJWGC+", "JukyoJusho")
-	fmt.Println(s1)
-	fmt.Println("--")
-	fmt.Println(s2)
+	// TODO CHECK REDIS STATUS
 }
